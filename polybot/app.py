42d6b73410d8fc8b3b921aff86e70b369481962d
Shweta Singh <shwetasng17@gmail.com>
2023-07-07 12:57:19 +0530

    modified polybot/app.py and yolo5/app.py

--- polybot/app.py
+++ polybot/app.py
@@ -74,14 +74,75 @@ class QuoteBot(Bot):
 
 
 class ObjectDetectionBot(Bot):
-    pass
+    def handle_message(self, message):
+        logger.info(f'Incoming message: {message}')
+
+        if message.chat.type == 'private' or ():
+            if self.is_current_msg_photo():
+                self.detect_objects()
+
+
+    def detect_objects(self):
+        photo_path = self.download_user_photo()
+
+        # Send the photo to the YOLO service for object detection
+        res = requests.post(f'{YOLO_URL}/predict', files={
+            'file': (photo_path, open(photo_path, 'rb'), 'image/png')
+        })
+
+        if res.status_code == 200:
+            detections = res.json()
+            logger.info(f'response from detect service with {detections}')
+
+            # calc summary
+            element_counts = Counter([l['class'] for l in detections])
+            summary = 'Objects Detected:\n'
+            for element, count in element_counts.items():
+                summary += f"{element}: {count}\n"
+
+            self.send_text_with_quote(summary, message_id=self.current_msg.message_id)
+
+            # Delete the photo file
+            os.remove(photo_path)
+
+        else:
+            self.send_text('Failed to perform object detection. Please try again later.')
+
+
+
+
 
 
 if __name__ == '__main__':
-    # TODO - in the 'polyBot' dir, create a file called .telegramToken and store your bot token there.
-    #  ADD THE .telegramToken FILE TO .gitignore, NEVER COMMIT IT!!!
+
     with open('.telegramToken') as f:
         _token = f.read()
 
-    my_bot = Bot(_token)
+    my_bot = ObjectDetectionBot(_token)
+
+
+    @my_bot.bot.message_handler(commands=['start'])
+    def handle_start(message):
+        my_bot.send_text('Welcome to the POLY-vision bot. Send an image or tag an image with /yolo to detect objects.')
+
+
+    @my_bot.bot.message_handler(commands=['help'])
+    def handle_help(message):
+        help_text = 'How to use the bot:\n\n' \
+                    '/start - Start the bot and get a welcome message\n' \
+                    '/help - Get instructions on how to use the bot\n' \
+                    '/yolo - Tag an image to detect objects'
+        my_bot.send_text(help_text)
+
+
+    @my_bot.bot.message_handler(commands=['yolo'])
+    def handle_yolo(message):
+        if message.reply_to_message and message.reply_to_message.photo:
+            my_bot.current_msg = message.reply_to_message
+            my_bot.detect_objects()
+        else:
+            my_bot.send_text('Please tag an image or send an image for object detection.')
+
+
+
     my_bot.start()
